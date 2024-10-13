ee6e1e8360926bbf554936c0d64deeb64b3f4bff
Sasha <alexandredepape@gmail.com>
2024-10-14 01:40:12 +0200

    build(deps): update package.json, app.module.ts and yarn.lock (#1)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
-import { RedisModule } from 'nestjs-redis';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestQueueProcessor } from './test-queue.processor';
@@ -14,12 +14,15 @@ const configModuleOptions = {
 			name: "test_queue",
 		}),
 		ConfigModule.forRoot(configModuleOptions),
-    RedisModule.register({
+    RedisModule.forRoot({
+			readyLog: true,
+			config: {
 				host: process.env.REDIS_HOST,
 				port: Number.parseInt(process.env.REDIS_PORT as string),
 				username: process.env.REDIS_USERNAME,
 				password: process.env.REDIS_PASSWORD,
 				family: Number.parseInt(process.env.REDIS_FAMILY as string),
+			},
 		}),
   ],
   controllers: [AppController],
