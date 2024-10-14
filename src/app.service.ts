e8f17b58e86117ee65f8e5e33a92619fa8414d9a
flazoo <alexandredepape@gmail.com>
2024-10-14 02:08:10 +0200

    update fly.toml and app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Queue } from 'bull';
 import { InjectQueue } from '@nestjs/bull';
 
 @Injectable()
 export class AppService {
-
+  private readonly logger = new Logger(AppService.name);
   constructor(
     @InjectQueue('test_queue') private testQueue: Queue
   ) {}
 
 
   async addJobToQueue(data: any): Promise<void> {
+    this.logger.log(`Adding job to queue: ${data}`);
     await this.testQueue.add('process_message', data, {
       attempts: 3,
       backoff: {
@@ -18,5 +19,6 @@ export class AppService {
         delay: 1000,
       },
     });
+    this.logger.log(`Job added to queue: ${data}`);
   }
 }
